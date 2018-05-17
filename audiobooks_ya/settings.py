da1f20ca8ae06e42b4fabbecbb15d52230f0cbce
Aleksei Kurylev <chepe4pi@gmail.com>
2018-05-17 12:15:25 +0300

    allow hosts

--- audiobooks_ya/settings.py
+++ audiobooks_ya/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=^mpbephh*grrx*g)3%$^e!9z81&lgbs@+vih10vm0rrhfgjf-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
